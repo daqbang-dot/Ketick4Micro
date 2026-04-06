271a983acbaaac50d29a9ea233e3555afe0f1f70
daqbang-dot <daqbang@gmail.com>
2026-04-07 01:10:08 +0800

    Update legend.js

--- plans/legend.js
+++ plans/legend.js
@@ -1,9 +1,16 @@
 // Konfigurasi akses untuk Pakej LEGEND
 export const LegendPlan = {
     planName: 'LEGEND',
-    maxProducts: Infinity, // Tiada had
+    maxProducts: Infinity,
     canUploadLogo: true,
     hasWatermark: false,
     enableWhatsAppReceipt: true,
-    enableSalesReports: true
+    enableSalesReports: true,
+    // MODUL BARU
+    enableCRM: true,
+    enableKupon: true,
+    enableLHDN: true,
+    enableWABlast: true,
+    enableBluetoothPrint: true,
+    enableBuku555: true
 };
