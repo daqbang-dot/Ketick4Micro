021abc00d0c1c78c69b4136fe3d4f4e48bc25b2d
daqbang-dot <daqbang@gmail.com>
2026-04-07 01:17:06 +0800

    Update basic.js

--- plans/basic.js
+++ plans/basic.js
@@ -4,5 +4,12 @@ export const BasicPlan = {
     canUploadLogo: false,
     hasWatermark: true,
     enableWhatsAppReceipt: false,
-    enableSalesReports: false
+    enableSalesReports: false,
+    // MODUL BARU
+    enableCRM: false,
+    enableKupon: false,
+    enableLHDN: false,
+    enableWABlast: false,
+    enableBluetoothPrint: false,
+    enableBuku555: false
 };
